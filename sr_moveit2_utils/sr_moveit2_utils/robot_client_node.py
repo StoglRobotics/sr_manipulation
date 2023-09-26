ab088b3ce61d219c7b938c97c7e7702d6399aa15
sahand_w <sahandwagemakers@gmail.com>
2023-09-26 15:18:02 +0200

    Modify the copyright and robot name

--- sr_moveit2_utils/sr_moveit2_utils/robot_client_node.py
+++ sr_moveit2_utils/sr_moveit2_utils/robot_client_node.py
@@ -1,12 +1,34 @@
-# Copyright (c) 2023 Stogl Robotics Consulting UG (haftungsbeschränkt)
-# All rights reserved.
+# BSD 3-Clause License
 #
-# Made for Hochschule Karlsruhe
+# Copyright (c) 2023, Stogl Robotics Consulting
 #
-# Proprietary License
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
 #
-# Unauthorized copying of this file, via any medium is strictly prohibited.
-# The file is considered confidential.
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# based on moveit_clients.py, scene_client 
+#
+# Author Dr Denis Stogl, Guillaume Walck
 
 from copy import deepcopy
 import time
@@ -634,7 +656,7 @@ def main(args=None):
 
     executor = MultiThreadedExecutor()
     
-    rc = RobotClient("maurob_robot", 1.0)
+    rc = RobotClient("robot_client", 1.0)
 
     try:
         rclpy.spin(rc, executor)
