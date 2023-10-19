17d4aeeeed6577e5f8fc8cf82a25f1618176cbc9
Dr. Denis Štogl <denis@stoglrobotics.de>
2023-10-19 23:20:03 +0200

    Better check for pyassimp installation and version.

--- sr_moveit2_utils/sr_moveit2_utils/scene_manager.py
+++ sr_moveit2_utils/sr_moveit2_utils/scene_manager.py
@@ -89,7 +89,8 @@ from shape_msgs.msg import SolidPrimitive, Mesh, MeshTriangle
 # original authors: Ioan Sucan, Felix Messmer, same License as above
 try:
     import pyassimp
-    from pyassimp import load
+    from pyassimp import load as pyassimp_load
+    from importlib.metadata import version as libversion
 
 except:
     pyassimp = False
@@ -108,6 +109,15 @@ class SceneManager(Node):
         :param scene_base_frame: Base frame of the scene (default: "world")
         """
         super().__init__(name)
+
+        # Check if correct version of pyassimp is used. There version have to be different then 4.1.4
+        if pyassimp is False:
+            self.get_logger().fatal("pyassimp library not found!")
+            return None
+        else:
+            if libversion('pyassimp') == '4.1.4':
+                self.get_logger().fatal("Pyassimp version '4.1.4' has a bug - please install a newer version, e.g., using 'pip3 install -U pyassimp'!")
+                return None
 
         # storage to track the objects
         self.object_in_the_scene_storage = {}  #dict[str, CollisionObject]
@@ -148,17 +158,14 @@ class SceneManager(Node):
     # make_mesh copied from planning_scene_interface.py of moveit_commander
     # original authors: Ioan Sucan, Felix Messmer, same License as above
     def make_mesh(self, uri: str, scale = (1, 1, 1)):
-        if pyassimp is False:
-            self.get_logger().warn("Pyassimp not found")
-            return None
         try:
             # TODO(gwalck) handle uri in a cleaner way, maybe even with packages
             if uri.startswith("file://"):
                 filename = uri[7:len(uri)]
             else:
                 filename = uri
-            #with load('/home/guillaumew/workspaces/kh_kuka/install/moveit_wrapper/share/moveit_wrapper/resources/brick_pocket.stl') as scene:
-            with load(filename) as scene:
+            #with pyassimp_load('/home/guillaumew/workspaces/kh_kuka/install/moveit_wrapper/share/moveit_wrapper/resources/brick_pocket.stl') as scene:
+            with pyassimp_load(filename) as scene:
                 if not scene.meshes or len(scene.meshes) == 0:
                     self.get_logger().warn("There are no meshes in the file")
                     return None
